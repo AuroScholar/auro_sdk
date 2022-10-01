963d7fe63610691ca8f586552b3954ae110943c8
user <manju.chaudhary@aurosociety.org>
2022-10-01 10:22:06 +0530

    Initial commit with Auroscholar complete module

---
+++ app/src/main/java/com/auroscholar/implementauroapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.auroscholar.implementauroapp;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+
+import com.auro.application.home.presentation.view.activity.SplashScreenAnimationActivity;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, SplashScreenAnimationActivity.class);
+        startActivity(intent);
+    }
+}
